e5a6fa7c49e29503cbbfcebff60517745956d886
Jolmas <jolmasch@gmail.com>
2025-09-02 18:13:33 -0400

    Update Wofi.sh

--- scripts/Wofi.sh
+++ scripts/Wofi.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+r#!/bin/bash
 
 CONFIG="$HOME/.config/wofi/config"
 STYLE="$HOME/.config/wofi/style.css"
 COLORS="$HOME/.config/wofi/colors"
 
-wofi --show drun --prompt 'Buscar...' --conf ${CONFIG} --style ${STYLE} --color ${COLORS}
+wofi --show drun --prompt 'Search...' --conf ${CONFIG} --style ${STYLE} --color ${COLORS}
